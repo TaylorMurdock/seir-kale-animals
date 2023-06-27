3c4028bc110bb4f5a45da4a578b39e55838cf33e
Taylor Murdock <tjmurd4545@gmail.com>
2023-06-27 17:04:50 -0400

    models folder set up

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const morgan = require("morgan");
 const app = express();
 
 app.get("/", (req, res) => {
-  //shows hello world on 4245 when running
+  //shows hello world on PORT when running
   res.send("hello world");
 });
 
+// listener
 app.listen(process.env.PORT, () =>
   console.log(`listening to port ${process.env.PORT}`)
 );
